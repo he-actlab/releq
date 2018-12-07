02f4cedc6ca0efbe1de129179ecf7aac4dd42c68
Amir Yazdanbakhsh <a.yazdanbakhsh@gatech.edu>
2018-12-07 03:09:12 -0800

    minor

--- models/svhn/svhn_simple.py
+++ models/svhn/svhn_simple.py
@@ -11,34 +11,38 @@ model_urls = {
 
 
 class SVHN(nn.Module):
-    def __init__(self):
+    def __init__(self, features, num_classes=10):
         super(SVHN, self).__init__()
-	#
-        n_channel = 32
-        n_classes = 10
-        cfg = [n_channel, n_channel, 'M', 2*n_channel, 2*n_channel, 'M', 4*n_channel, 4*n_channel, 'M', (8*n_channel, 0), 'M']
-        layers = []
-        in_channels = 3
-        for i, v in enumerate(cfg):
-            if v == 'M':
-                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
-            else:
-                padding = v[1] if isinstance(v, tuple) else 1
-                out_channels = v[0] if isinstance(v, tuple) else v
-                conv2d = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=padding)
-                # with batchnorm
-                layers += [conv2d, nn.BatchNorm2d(out_channels, affine=False), nn.ReLU(), nn.Dropout(0.3)]
-                in_channels = out_channels
-        self.model = nn.Sequential(*layers)
-        #
-        self.classifier = nn.Sequential(nn.Linear(8*n_channel, n_classes))
+	    #
+        self.features = features
+        self.classifier = nn.Linear(256, num_classes)
         #
     def forward(self, x):
-        x = self.model(x)
+        x = self.features(x)
         x = x.view(x.size(0), -1)
         x = self.classifier(x)
         return x
 
+def make_layers(cfg, batch_norm=False):
+    layers = []
+    in_channels = 3
+    for i, v in enumerate(cfg):
+        if v == 'M':
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        else:
+            padding = v[1] if isinstance(v, tuple) else 1
+            out_channels = v[0] if isinstance(v, tuple) else v
+            conv2d = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=padding)
+            if batch_norm:
+                layers += [conv2d, nn.BatchNorm2d(out_channels, affine=False), nn.ReLU(), nn.Dropout(0.3)]
+            else:
+                layers += [conv2d, nn.ReLU(), nn.Dropout(0.3)]
+            in_channels = out_channels
+    return nn.Sequential(*layers)
+
+n_channel = 32
+cfg = [n_channel, n_channel, 'M', 2*n_channel, 2*n_channel, 'M', 4*n_channel, 4*n_channel, 'M', (8*n_channel, 0), 'M']
+
 def svhn(**kwargs):
-    model = SVHN()
+    model = SVHN(make_layers(cfg), **kwargs)
     return model
