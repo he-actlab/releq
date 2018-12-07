aed76c0b7dd07b452ce4a0bc26a2377cb2e0ad58
Prannoy Pilligundla <prannoyp.1994@gmail.com>
2018-12-06 20:24:35 -0800

    Changed layer dim for Lenet

--- models/mnist/lenet_mnist.py
+++ models/mnist/lenet_mnist.py
@@ -22,12 +22,11 @@ __all__ = ['lenet_mnist']
 class Lenet(nn.Module):
     def __init__(self):
         super(Lenet, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.conv1 = nn.Conv2d(1, 20, 5)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.conv2 = nn.Conv2d(20, 50, 5)
+        self.fc1 = nn.Linear(800, 500)
+        self.fc2 = nn.Linear(500, 10)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
@@ -36,7 +35,6 @@ class Lenet(nn.Module):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         #x = nn.Threshold(0.2, 0.0)#ActivationZeroThreshold(x)
-        x = self.fc3(x)
         return x
 
 def lenet_mnist():
