Fixed GAEs normalization issue

examples/classifier_compression/ppo_all_layers.py
@@ -290,7 +290,7 @@ class RLQuantization:
         self.num_layers = num_layers # number of layers in the NN that needs to be Optimized
         self.n_act_p_episode     = 1  # number of actions per each episod (fix for now)
         #self.total_episodes        = num_episodes  # total number of observations used for training (in order)
-        self.total_episodes = 2500
+        self.total_episodes = 1500
         self.network_name     = network_name  # defines the network name
 
         #self.supported_bit_widths = self.yaml_config["supported_bitwidths"] #[2, 3, 4, 5, 8] #[2, 3, 4, 5, 8]
@@ -582,9 +582,8 @@ class RLQuantization:
         reward = z[x][y]
         if reward <= 0:
             reward = 0
-        return reward*3
-    
- 
+        return reward
+
     def calculate_reward(self, cur_accuracy, prev_accuracy, cur_bitwidth, new_bitwidth):
         print("Acc Diff", cur_accuracy - prev_accuracy)
         acc_reward = (cur_accuracy - prev_accuracy)*self.acc_reward_const
@@ -713,14 +712,15 @@ class RLQuantization:
                         rewards = np.array(rewards).astype(dtype=np.float32)
                         v_preds_next = np.array(v_preds_next).astype(dtype=np.float32)
                         gaes = np.array(gaes).astype(dtype=np.float32)
-                        if num_layers_together != 1:
-                            gaes = (gaes - gaes.mean()) / gaes.std()
+                        #if num_layers_together != 1:
+                        #    gaes = (gaes - gaes.mean()) / gaes.std()
                         
                         self.PPO.assign_policy_parameters()
                         
                         inp = [observations, actions, rewards, v_preds_next, gaes]
                         #print(inp)
-                        
+                        #print(rewards, gaes, v_preds_next, v_preds)
+
                         # train
                         for epoch in range(1):
                             #sample_indices = np.random.randint(low=0, high=observations.shape[0], size=num_layers_together)  # indices are in [low, high)
@@ -754,15 +754,18 @@ acc_cache = {}
 headers = ['episode_num', 'layer_num', 'quant_state', 'acc_state', 'reward',
                         'l1-bits', 'l2-bits', 'l3-bits', 'l4-bits',
                         'prob_2bits','prob_3bits', 'prob_4bits', 'prob_5bits', 'prob_8bits']
-
-network_name = "lenet"
-number_of_layers = 4
-file_name = "lenet_learning_history_log.csv"
+network_name = "svhn"
+number_of_layers = 8
+file_name = "releq_svhn_learning_history_log.csv"
 layer_info = StringIO("""layer_idx_norm;n;c;k;std
-1;20;1;5;0.18183
-2;50;20;5;0.03791
-3;500;800;0;0.02124
-4;10;500;0;0.06587""")
+1;32;3;3;0.18325
+2;32;32;3;0.04787
+3;64;32;3;0.04403
+4;64;64;3;0.03448
+5;128;64;3;0.03441
+6;128;128;3;0.02876
+7;256;128;3;0.02559
+8;10;256;0;0.09887""")
 with open(file_name, 'w') as writeFile:
     writer = csv.writer(writeFile)
     writer.writerow(headers)
@@ -778,10 +781,10 @@ for layer in range(number_of_layers):
     layer_state_info.loc[layer, 'c'] = (layer_state_info.loc[layer, 'c'] - min_c)/(max_c - min_c)
     layer_state_info.loc[layer, 'k'] = (layer_state_info.loc[layer, 'k'] - min_k)/(max_k - min_k)
 print(layer_state_info)
-layer_names = ["conv1", "conv2", "fc1", "fc2"]
-training_cmd = "python3 compress_classifier.py --arch lenet_mnist ../../../data.mnist --quantize-eval --compress ./lenet_bn_dorefa.yaml --epochs 5 --lr 0.001 --resume ./lenet_mnist.pth.tar"
-yaml_file = "lenet_bn_dorefa.yaml"
-accuracy_cache_file = "lenet_accuracy_cache.txt"
-quant_type = "dorefa_quantizer"
-rl_quant = RLQuantization(number_of_layers, 99.8, network_name, layer_names, layer_state_info, training_cmd, yaml_file, quant_type) #num_layers, accuracy, network_name, layer_names, layer_stats
+layer_names = ["features.0", "features.3", "features.7", "features.10", "features.14", "features.17", "features.21", "classifier.0"]
+training_cmd = "python3 compress_classifier.py --arch svhn ../../../data.svhn --quantize-eval --compress svhn_bn_wrpn.yaml --epochs 5 --lr 0.01 --resume svhn.pth.tar"
+yaml_file = "svhn_bn_wrpn.yaml"
+accuracy_cache_file = "svhn_accuracy_cache.txt"
+quant_type = "wrpn_quantizer"
+rl_quant = RLQuantization(number_of_layers, 97, network_name, layer_names, layer_state_info, training_cmd, yaml_file, quant_type) #num_layers, accuracy, network_name, layer_names, layer_stats
 rl_quant.quantize_layers_together(number_of_layers)